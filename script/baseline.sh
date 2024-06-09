2adf92f0712e7840f67fd272b1d35f4f62e0ee0c
陳俞靜 <chenyujing@chenyujingdeMacBook-Pro.local>
2024-06-09 17:43:54 +0800

    script_add_dim

--- script/baseline.sh
+++ script/baseline.sh
@@ -2,7 +2,6 @@
 
 # Parameters
 ALGORITHM="simple_gp"
-DIM=10
 POP_SIZE=500
 CX_METHOD="cx_simple"
 MAX_EVAL=100000
@@ -13,11 +12,14 @@ EMBEDDING_TYPE="word2vec"
 # python main.py -algo $ALGORITHM -e $EMBEDDING_TYPE -n $DIM -p $POP_SIZE -pc 1 -pm 0 -g $MAX_GEN -c $CX_METHOD -eval $MAX_EVAL
 
 # Running experiments
-for PC in 0 0.1 0.5 0.9
+for DIM in 10 15 20 25 50 100
 do
-    for PM in 0 0.01 0.03 0.05 0.1 0.5
+    for PC in 1 0 0.1 0.5 0.9
     do
-        echo "Running $EMBEDDING_TYPE with pc=$PC and pm=$PM"
-        python ../main.py -algo $ALGORITHM -e $EMBEDDING_TYPE -n $DIM -p $POP_SIZE -pc $PC -pm $PM -c $CX_METHOD -eval $MAX_EVAL
+        for PM in 0 0.01 0.03 0.05 0.1 0.5
+        do
+            echo "Running $EMBEDDING_TYPE with dim=$DIM pc=$PC and pm=$PM"
+            python ../main.py -algo $ALGORITHM -e $EMBEDDING_TYPE -n $DIM -p $POP_SIZE -pc $PC -pm $PM -c $CX_METHOD -eval $MAX_EVAL
+        done
     done
 done
