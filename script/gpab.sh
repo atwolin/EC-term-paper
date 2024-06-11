782c568e0fb4fd9b6c50dbd3a18d791f5a339513
atwolin <tzuchien@nlplab.cc>
2024-06-11 10:09:17 +0800

    upload results

--- script/gpab.sh
+++ script/gpab.sh
@@ -14,11 +14,11 @@ for EMBEDDING_TYPE in "word2vec" "glove" "fasttext"
 do
     for DIM in 10 15 20 25 50 100
     do
-        for PC in 1 0.1 0.9
+        for PC in 1 0.1 0.3 0.7 0.9
         do
-            for PM in 0.1 0 0.03  0.5
+            for PM in 0.1 0 0.03 0.3 0.5
             do
-                for MAX_GEN in 100 150 200
+                for MAX_GEN in 100 50 150 200 250
                 do
                     echo "Running $EMBEDDING_TYPE with dim=$DIM pc=$PC and pm=$PM"
                     python ../main.py -algo $ALGORITHM -e $EMBEDDING_TYPE -n $DIM -p $POP_SIZE -pc $PC -pm $PM -c $CX_METHOD -g $MAX_GEN
